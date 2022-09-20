35937393a7fd2b52d5fd5bbc4d146fec97bb528f
benali-chadi <benalioulhajchadi@gmail.com>
2022-09-20 03:24:01 +0000

    search filters

--- src/components/search/Filters.tsx
+++ src/components/search/Filters.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from "react";
 // import { FilterInter } from "../../../pages/search";
 import { BsChevronDown } from "react-icons/bs";
 import SortFilter from "./filters/SortFilter";
+import StatusFilter from "./filters/StatusFilter";
 
 const Filters = () => {
 	return (
@@ -12,10 +13,7 @@ const Filters = () => {
 					<h3 className="text-xl font-bold">Year</h3>
 					<BsChevronDown />
 				</div>
-				<div className="flex gap-2 items-center p-2 text-black bg-white rounded-xl cursor-pointer hover:bg-white/80">
-					<h3 className="text-xl font-bold">Status</h3>
-					<BsChevronDown />
-				</div>
+				<StatusFilter />
 				<div className="flex gap-2 items-center p-2 text-black bg-white rounded-xl cursor-pointer hover:bg-white/80">
 					<h3 className="text-xl font-bold">Genres</h3>
 					<BsChevronDown />
--- src/components/search/filters/StatusFilter.tsx
+++ src/components/search/filters/StatusFilter.tsx
@@ -1,8 +1,13 @@
 import React, { useRef, useState } from "react";
+import { useContext } from "react";
 import { BsChevronDown } from "react-icons/bs";
 import { useOutsideClick } from "../../../lib/useOutsideClick";
+import { FilterContext, FilterContextState } from "../../helpers/context";
+import { DropDownHeader } from "./SortFilter";
 
 const StatusFilter = () => {
+	const { filters, setFilters } =
+		useContext<FilterContextState>(FilterContext);
 	const [toggle, setToggle] = useState(false);
 	const [title, setTitle] = useState("Status");
 	const myRef = useRef(null);
@@ -22,20 +27,59 @@ const StatusFilter = () => {
 			</div>
 			{/* Sort filters */}
 			{toggle && (
-				<></>
-				// <SortDropDown
-				// 	setFilters={setFilters}
-				// 	filters={filters}
-				// 	hide={() => {
-				// 		setToggle(false);
-				// 	}}
-				// 	changeTitle={setTitle}
-				// />
+				<StatusDropDown
+					setFilters={setFilters}
+					filters={filters}
+					hide={() => {
+						setToggle(false);
+					}}
+					changeTitle={setTitle}
+				/>
 			)}
 		</div>
 	);
 };
 
-function statusDropDown({}) {}
+function StatusDropDown({ setFilters, filters, hide, changeTitle }) {
+	return (
+		<div
+			className={`flex overflow-auto absolute left-0 flex-col gap-2 p-2 w-max bg-gray-200 rounded-xl top-[50px] max-h-[15rem]`}
+		>
+			<DropDownHeader
+				title="airing"
+				handleClick={() => {
+					setFilters({
+						...filters,
+						state: "airing",
+					});
+					changeTitle("airing");
+					hide();
+				}}
+			/>
+			<DropDownHeader
+				title="complete"
+				handleClick={() => {
+					setFilters({
+						...filters,
+						state: "complete",
+					});
+					changeTitle("complete");
+					hide();
+				}}
+			/>
+			<DropDownHeader
+				title="upcoming"
+				handleClick={() => {
+					setFilters({
+						...filters,
+						state: "upcoming",
+					});
+					changeTitle("upcoming");
+					hide();
+				}}
+			/>
+		</div>
+	);
+}
 
 export default StatusFilter;
